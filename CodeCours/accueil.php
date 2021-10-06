3e1c9f5fa496ad58ecf0ccd7a8af9c25b8dba99e
choquitofrito <zyriab@gmail.com>
2021-10-06 11:59:00 +0200

    ok

--- CodeCours/accueil.php
+++ CodeCours/accueil.php
@@ -10,17 +10,31 @@
 
 <body>
     <?php
-        include "./Personne.class.php";
+    include "./Personne.class.php";
+    include "./PersonneC.class.php";
+    include "./PersonneNC.class.php";
 
-        // Personne p1 = new Personne(); // java, c#....
+    // Personne p1 = new Personne(); // java, c#....
 
-        $p1 = new Personne();
-        $p1->setNom ("Lila");
-        $p1->setPrenom ("Lula");
-        var_dump ($p1);
-        
+    $p1 = new Personne();
+    $p1->setNom("Lila");
+    $p1->setPrenom("Lula");
+    var_dump($p1);
 
+    $p2 = new Personne();
+    $p2->setNom("Hermant");
+    $p2->setPrenom("Margaret");
+    var_dump($p2);
 
+    $p3 = new PersonneC("Bowie","David");
+    var_dump ($p3);
+    $p4 = new PersonneC();
+    var_dump ($p4);
+
+    $p5 = new PersonneNC("Jackson","Michael");
+    var_dump ($p5);
+    $p6 = new PersonneNC();
+    var_dump ($p6);
 
     ?>
 </body>

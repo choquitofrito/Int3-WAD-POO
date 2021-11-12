3524653274068887a0c6b7865c085140b0a74596
choquitofrito <zyriab@gmail.com>
2021-11-12 09:58:04 +0100

    start aggregation

---
+++ Notes/Aggregation/Contact.class.php
@@ -0,0 +1,53 @@
+<?php
+
+class Contact {
+
+    public string $nom;
+    public string $prenom;
+
+    public function __construct(string $nom, string $prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * Get the value of nom
+     */ 
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @return  self
+     */ 
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     */ 
+    public function getPrenom()
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @return  self
+     */ 
+    public function setPrenom($prenom)
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+}

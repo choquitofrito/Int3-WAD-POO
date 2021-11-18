b59260858e240cdee12b5ac780fd09c27558d781
choquitofrito <zyriab@gmail.com>
2021-11-18 11:44:56 +0100

    ok

--- Notes/Namespaces/ExemplesCours/accueil2-use.php
+++ Notes/Namespaces/ExemplesCours/accueil2-use.php
@@ -14,18 +14,15 @@
     include "./vendor/autoload.php";
 
     use \Shop\Basket\Product as ProduitPanier; // use nous permet de ne pas devoir
-                            // taper le nom du namespace complet 
+                                            // taper le nom du namespace complet 
 
     use \Shop\BasketFake\Product as ProductPanierFaux;
 
     $p1 = new ProduitPanier(4); // charge Produit de Basket (use plus haut)
-
     $pf1 = new ProductPanierFaux(5);
 
     // on pourrait faire un use aussi pour Basket
     // car le namespace est déjà spécifié dans la classe
-    $b1 = new \Shop\Basket\Basket(5);
-
 
     ?>
 </body>

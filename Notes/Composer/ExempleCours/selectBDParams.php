1052d3a4973130fa6d23749752fab0b29dc2d06d
choquitofrito <zyriab@gmail.com>
2021-11-18 15:52:14 +0100

    ok

--- Notes/Composer/ExempleCours/selectBDParams.php
+++ Notes/Composer/ExempleCours/selectBDParams.php
@@ -9,9 +9,12 @@
 </head>
 
 <body>
-    <?php
+<?php
 
-    include "./config/db.php";
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
+
+include "./config/db.php";
 
     try {
         $bdd = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT .
@@ -20,12 +23,17 @@
     } catch (PDOException $e) {
         echo "Une erreur de connexion s'est produite <a href='./accueil.php'>Accueil</a>";
         // echo $e->getMessage();
+        
+        // ici, rajouter un message dans le fichier de logging si il y a une erreur
+        $monLogger = new Logger("monLogger");
+        $monLogger->pushHandler( new StreamHandler("log.txt",Logger::INFO));
+        $monLogger->info("Une erreur s'est produite dans la connexion");
         die();
     }
     // toutes les villes d'un pays
     $sql = "SELECT country.country, city.city FROM country ".
             "INNER JOIN city ".
-            "ON country.country_id = city.country_id ".
+            "ON country.id = city.city_id ".
             "WHERE country.country = :unPays";
     $requete = $bdd->prepare($sql);
     $requete->bindValue(":unPays", "Italy");
---
+++ Notes/Composer/ExempleCours/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'a1f788a24f219380d71944b9a7ee2ce7821eb002',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'a1f788a24f219380d71944b9a7ee2ce7821eb002',
+            'dev_requirement' => false,
+        ),
+        'monolog/monolog' => array(
+            'pretty_version' => '1.0.2',
+            'version' => '1.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../monolog/monolog',
+            'aliases' => array(),
+            'reference' => 'b704c49a3051536f67f2d39f13568f74615b9922',
+            'dev_requirement' => false,
+        ),
+    ),
+);

eb3511b28498cc9546afd02655f0ed3134a188ec
choquitofrito <zyriab@gmail.com>
2021-11-18 14:34:17 +0100

    static

--- Notes/Static/accueil.php
+++ Notes/Static/accueil.php
@@ -17,11 +17,13 @@
     // si public : 
     // Medecin::$codeDeo = "Soigner tout le monde";
     // echo Medecin::$codeDeo;
+
         
     // si private, besoin de get et set
     Medecin::setCodeDeo ("Vaccinez vous je dis!");
     echo "<br>Le code deontologique est: ";
     echo Medecin::getCodeDeo();
+
     
 
 

50354a3589276a89e780f091cc40846122a18f6f
choquitofrito <zyriab@gmail.com>
2023-09-04 16:44:04 +0200

    modele base insert

---
+++ NotesCours/4.CRUD-POO/accueil.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    include "./config/db.php";
+
+    try {
+        $cnx = new PDO(DBDRIVER . ':host=' . DBHOST . ';port=' . DBPORT . ';dbname=' . DBNAME . ';charset=' . DBCHARSET, DBUSER, DBPASSWORD);
+    } catch (Exception $e) {
+        // ce code sera lancé s'il y a une erreur de connexion
+        echo "Erreur de connexion à la BD";
+        die();
+    }
+
+    require_once "./Film.php";
+    require_once "./FilmManager.php";
+
+    $f1 = new Film("Alien", 120);
+    $f2 = new Film("Tron", 130);
+    $filmManager = new FilmManager($cnx);
+    $filmManager->insert($f1);
+    $filmManager->insert($f2);
+
+
+    ?>
+
+
+</body>
+
+</html>

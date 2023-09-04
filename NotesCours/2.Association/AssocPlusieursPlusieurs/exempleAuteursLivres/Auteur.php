4cb4facbeb75c91196341a37d75944fb33546670
choquitofrito <zyriab@gmail.com>
2023-09-04 13:55:47 +0200

    exemple many many no att

---
+++ NotesCours/2.Association/AssocPlusieursPlusieurs/exempleAuteursLivres/Auteur.php
@@ -0,0 +1,94 @@
+<?php
+
+
+class Auteur
+{
+    private string $nom;
+    private string $prenom;
+
+    // association
+    private array $livres;
+
+    public function __construct(string $nom, string $prenom)
+    {
+        $this->nom = $nom;
+        $this->prenom = $prenom;
+    }
+
+    /**
+     * Get the value of nom
+     *
+     * @return string
+     */
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @param string $nom
+     *
+     * @return self
+     */
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of prenom
+     *
+     * @return string
+     */
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    /**
+     * Set the value of prenom
+     *
+     * @param string $prenom
+     *
+     * @return self
+     */
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of livres
+     *
+     * @return array
+     */
+    public function getLivres(): array
+    {
+        return $this->livres;
+    }
+
+    /**
+     * Set the value of livres
+     *
+     * @param array $livres
+     *
+     * @return self
+     */
+    public function setLivres(array $livres): self
+    {
+        $this->livres = $livres;
+
+        return $this;
+    }
+
+    // rajouter les éléments de la collection
+    public function addLivre (Livre $livre){
+        
+    }
+}
---
+++ NotesCours/2.Association/AssocPlusieursPlusieurs/exempleAuteursLivres/accueil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    require_once "./Auteur.php";
+    require_once "./Livre.php";
+
+    $auteur1 = new Auteur("Woolf", "Virginia");
+    $auteur2 = new Auteur("Austen", "Jane");
+    
+    $livre1 = new Livre ("23432492349234", "Mrs. Dalloway");
+    $livre2 = new Livre ("23432492349234", "Les Vagues");
+
+    $livre3 = new Livre ("00234023402349", "Orgueil et préjugés");
+
+    
+
+
+
+
+    ?>
+</body>
+</html>

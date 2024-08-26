06603cda886dc5bbcdfc6465d767ddae158847c7
choquitofrito <zyriab@gmail.com>
2024-08-26 14:54:28 +0200

    findAll

--- NotesCours/FilmsOOP/classes/FilmManager.php
+++ NotesCours/FilmsOOP/classes/FilmManager.php
@@ -1,36 +1,65 @@
 <?php
+include "./connexion/db.php";
 
 class FilmManager
 {
+    private PDO $cnx;
 
-
-
-
-    public function insert(Film $film)
+    public function __construct()
     {
 
-        include "./connexion/db.php";
-
         try {
-            $cnx = new PDO(DSN, USERNAME, PASSWORD);
+            $this->cnx = new PDO(DSN, USERNAME, PASSWORD);
         } catch (Exception $e) {
             echo "Erreur de connexion";
             die();
         }
-
+    }
+    public function insert(Film $film)
+    {
         $sql = "INSERT INTO film (id, titre, duree, description, dateSortie, image) VALUES " .
             "(null, :titre, :duree, :description, :dateSortie, :image)";
 
-        $stmt = $cnx->prepare($sql);
-        $stmt->bindValue(":titre", $film->getTitre() );
-        $stmt->bindValue(":duree",$film->getDuree());
-        $stmt->bindValue(":description",$film->getDescription() );
-        $stmt->bindValue(":dateSortie", $film->getDateSortie() );
+        $stmt = $this->cnx->prepare($sql);
+        $stmt->bindValue(":titre", $film->getTitre());
+        $stmt->bindValue(":duree", $film->getDuree());
+        $stmt->bindValue(":description", $film->getDescription());
+        $stmt->bindValue(
+            ":dateSortie",
+            $film->getDateSortie()->format("Y-m-d")
+        );
         $stmt->bindValue(":image", $film->getImage());
 
         $stmt->execute();
+        $id = $this->cnx->lastInsertId(); // on récupére l'id du dernier enregistrement inséré dans la BD
+        $film->setId($id);
+    }
+
+    public function delete(Film $film):void
+    {
+        $sql = "DELETE FROM film WHERE id=:id";
+        $stmt = $this->cnx->prepare($sql);
+        $stmt->bindValue(":id", $film->getId());
+        $stmt->execute();
+    }
+
+    public function findAll():array
+    {
+        $sql = "SELECT * FROM film";
+        $stmt = $this->cnx->prepare($sql);
+        $stmt->execute();
+        $arrayArraysFilm = $stmt->fetchAll(PDO::FETCH_ASSOC); // un array d'arrays contenant les infos de chaque film. Pas d'objets!!!
 
 
-        
+        $arrayObjetsFilm = []; // notre array d'objets film
+
+        foreach ($arrayArraysFilm as $arrayFilm) {
+            $arrayFilm['dateSortie'] = 
+                    new DateTime ($arrayFilm['dateSortie']);
+            $arrayObjetsFilm[] = new Film($arrayFilm);
+        }
+
+
+        return ($arrayObjetsFilm);
     }
 }
--- NotesCours/FilmsOOP/testFilm.php
+++ NotesCours/FilmsOOP/testFilm.php
@@ -17,7 +17,9 @@
         [
             'titre' => 'Coco',
             'description' => 'film sur le Mexique',
-            'duree' => 100
+            'duree' => 100,
+            'dateSortie' => new DateTime(),
+            'image' => ""
         ]
     );
 
@@ -28,14 +30,18 @@
     // ]);
 
     $filmManager = new FilmManager();
+
+    
+    // var_dump ($film1);
+
     $filmManager->insert ($film1);
 
+    // var_dump ($film1);
+    
+    // effacer le film de la BD
+    $filmManager->delete ($film1);
 
-    var_dump ($film1);
-
-
-
-
+    var_dump($filmManager->findAll()); // renvoyer tous les films
 
 
 
